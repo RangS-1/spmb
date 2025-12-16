872be5b0c839ca945bd606fe25c37158e6d9ffbd
RangS <rangga19sj@gmail.com>
2025-12-16 20:25:23 +0700

    Refactor password confirmation and registration logic

--- asset/php/save.php
+++ asset/php/save.php
@@ -1,27 +1,26 @@
 <?php
 include "connect.php";
 
-$name         = $_POST['name'];
-$email        = $_POST['email'];
-$username     = $_POST['username'];
-$password     = $_POST['password'];
+$name     = $_POST['name'];
+$email    = $_POST['email'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 $con_password = $_POST['con_password'];
 
-// cek konfirmasi password
-if ($password != $con_password) {
-    echo "Password dan Konfirmasi Password tidak sama!";
+// validasi konfirmasi password
+if ($password !== $con_password) {
+    echo "Password dan konfirmasi tidak sama!";
     exit;
 }
 
-// enkripsi password (disarankan)
-$hash_password     = password_hash($password, PASSWORD_DEFAULT);
-$hash_con_password = password_hash($con_password, PASSWORD_DEFAULT);
+// hash password SEKALI SAJA
+$hash_password = password_hash($password, PASSWORD_DEFAULT);
 
-$query = "INSERT INTO user (name, email, username, password, con_password)
-          VALUES ('$name', '$email', '$username', '$hash_password', '$hash_con_password')";
+$query = "INSERT INTO user (name, email, username, password)
+          VALUES ('$name', '$email', '$username', '$hash_password')";
 
 if (mysqli_query($koneksi, $query)) {
-    echo "Data berhasil disimpan, Yattaaaa!";
+    echo "Registrasi berhasil! Yattaaa!";
 } else {
     echo "Error: " . mysqli_error($koneksi);
 }
